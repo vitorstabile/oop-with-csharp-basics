a2bb1b561797c9408bc69f11d692c928b5497ec0
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-14 21:25:22 -0300

    Push class 5

---
+++ class5-basic-structure-csharp-program/application/application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // reference to namespace system. Ex: To use Console.WriteLine, you have to use System
+
+namespace application // this program is part of namespace. Like package in Java
+{
+    class Program //Every source code in csharp, java have to be in a class
+    {
+        static void Main(string[] args) // When the execution program begin
+        {
+            Console.WriteLine("Hello World!"); // the code
+        }
+    }
+}
