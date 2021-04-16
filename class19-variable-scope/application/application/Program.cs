4022ac250c47e0699ed58befc033604bca7b802b
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-16 20:51:39 -0300

    Push class 16,17,18,19 and 20

---
+++ class19-variable-scope/application/application/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            int x;
+
+            Console.WriteLine(x);
+            
+            Wrong: you need to put a value in this variable to use
+
+             */
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+
+            /*
+
+            double preco = double.Parse(Console.ReadLine());
+            if (preco > 100.0)
+            {
+                double desconto = preco * 0.1;
+            }
+            
+            // Console.WriteLine(desconto); Wrong -> The variable desconto is in the scope of if
+
+            */
+
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);
+        }
+    }
+}
