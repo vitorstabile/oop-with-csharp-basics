a7a791004eaf2c24abb0d74c0d4f7b79cc6bc690
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-16 17:34:17 -0300

    push class 11

--- class10-assignment-operators-in-csharp/application/application/Program.cs
+++ class10-assignment-operators-in-csharp/application/application/Program.cs
@@ -22,6 +22,20 @@ namespace application
             s += "DEF";
 
             Console.WriteLine(s);
+
+            a = 10;
+            a++;
+            Console.WriteLine(a);
+
+            a = 10;
+            int b = a++; // difference -> b = a and a = 11
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = 10;
+            b = ++a; // a recive 10 + 1 and b = 11
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
---
+++ class11-convertion-and-casting-in-csharp/application/application/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+
+            float x = 4.5f;
+
+            double y = x; // can be convert -> double is bigger than float
+
+            Console.WriteLine(y);
+
+            double a;
+
+            float b;
+
+            a = 5.1;
+            // b = a; float is smaller than double. To make this operation you have to make a casting
+
+            b = (float)a; // casting
+
+            Console.WriteLine(b);
+
+            double c;
+            int d;
+
+            c = 5.1;
+            // d = a; int is smaller than double. To make this operation you have to make a casting, but you will lose information
+
+            d = (int)c; // casting
+
+            Console.WriteLine(d);
+
+            int e = 5;
+            int f = 2;
+
+            double result = e / f; // two division with int, dont make a double result. we have to make a casting
+
+            Console.WriteLine(result);
+
+            result = (double) e / f; // casting to result a double from division of two int`s
+
+            Console.WriteLine(result);
+        }
+    }
+}
