2a997cced8a445f2abbae5cb0d0a22773c3d135c
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-16 12:05:33 -0300

    Push class 6

---
+++ class6-data-types-in-csharp-1/application/application/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            SByte y = 127; // pre-definiton .net variable SByte - System biblio
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            byte n1 = 126; // can`t use negative values
+            int n2 = 1000;
+            int n3 = 2147483647; // max value of int
+            long n4 = 2147483648;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
