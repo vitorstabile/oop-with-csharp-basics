4d183c0773680ff7cf983c14dee3c14a25e69e6b
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-18 23:29:23 -0300

    Push class 28 and 29

---
+++ class28-csharp-problem-with-oop/application/application/Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application
+{
+    class Triangulo{
+
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
