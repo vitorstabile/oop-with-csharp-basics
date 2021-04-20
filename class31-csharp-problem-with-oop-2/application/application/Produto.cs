a8ea85f4ff89cd4e2c9a7587bd99999b524b02a2
Vitor Garcia <vitor.stabile@hotmail.com>
2021-04-19 22:48:05 -0300

    Push class 31

---
+++ class31-csharp-problem-with-oop-2/application/application/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            this.Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            this.Quantidade -= quantidade;
+        }
+    }
+}
---
+++ class31-csharp-problem-with-oop-2/application/application/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os Dados do Produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Dados do Produto: ");
+            Console.WriteLine(p.Nome + ", $ " + (p.Preco).ToString("F2", CultureInfo.InvariantCulture) + ", " + p.Quantidade.ToString() + " unidades, Total: $ " + (p.ValorTotalEmEstoque()).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.Write("Dados atualizados: ");
+            Console.WriteLine(p.Nome + ", $ " + (p.Preco).ToString("F2", CultureInfo.InvariantCulture) + ", " + p.Quantidade.ToString() + " unidades, Total: $ " + (p.ValorTotalEmEstoque()).ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.Write("Dados atualizados: ");
+            Console.WriteLine(p.Nome + ", $ " + (p.Preco).ToString("F2", CultureInfo.InvariantCulture) + ", " + p.Quantidade.ToString() + " unidades, Total: $ " + (p.ValorTotalEmEstoque()).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
